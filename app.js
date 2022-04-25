dc1a5459925d8f0ae52ece78c8e32eb1b62f5063
Mariah <mariahschock@gmail.com>
2022-04-25 15:42:43 -0700

    js/html updates

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // import functions and grab DOM elements
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
 
 // let state
 
@@ -6,3 +9,6 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+btn1.addEventListener('click', () => {
+
+});
